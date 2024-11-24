029cd9505bf7d94f70dd2fa1d34f6912443b9583
Mészáros Dominik <mdlolxd01@gmail.com>
2024-11-24 17:58:51 +0100

    fixed category update query

--- src/main/kotlin/com/mesdominik/expensetracker/CategoryRepository.kt
+++ src/main/kotlin/com/mesdominik/expensetracker/CategoryRepository.kt
@@ -2,12 +2,16 @@ package com.mesdominik.expensetracker
 
 import com.mesdominik.expensetracker.entity.Category
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
+import org.springframework.transaction.annotation.Transactional
 
 @Repository
 interface CategoryRepository : JpaRepository<Category, Long> {
+    @Modifying
+    @Transactional
     @Query(value = "update category set name=:name where id=:id;", nativeQuery = true)
     fun updateCategory(@Param("id") id: Long, @Param("name") name: String)
 }
